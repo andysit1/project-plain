ec1a9d40f80e4457846b3d0b6b235c63eddbb561
Andy Jun-Lok Sit <asit5@uwo.ca>
2024-09-26 16:41:26 -0400

    added example for adding nodes

--- web/diagram/index.js
+++ web/diagram/index.js
@@ -3,15 +3,40 @@
 
 
 import Graph from "./components/graph.js"
-import {Rect, State} from "./components/node.js"
+import { Rect, State } from "./components/node.js"
+import { randomInt } from "./utils/nums.js"
 
 const NODE_WIDTH = 150
 const NODE_HEIGHT = 75
 
+//let's create a class which holds the target state and represent the 
+//connection to toolbar html.. #GOOGLE THIS
+
 //TODO CRUD METHODS , toolbar class?
-function add_node(){
+function add_node(states){
+    //create a base nod
+    console.log(states)
+
+    let startX = randomInt(0, 400)
+    let startY = randomInt(0, 400)
+
+    const rect = new Rect(
+      startX,
+      startY,
+      NODE_WIDTH,
+      NODE_HEIGHT
+    )
+        
+    const stateNode = new State(states.length, " Node", rect)
+    states.push(stateNode)
+
 }
-function delete_node(){
+
+function delete_node(states){
+  let targetStates
+  for (const state of states) {
+    if (state.isInBounds(x, y)) { targetState = state }
+  }
 }
 
 
@@ -43,10 +68,17 @@ function init(){
 
     const stateNode = new State(0, "Home", rect)
     const stateNode1 = new State(1, "Origin", rect2)
+    stateNode.activeState = true
 
     graph.states.push(stateNode)  
     graph.states.push(stateNode1)
     graph.repaint = true
+  
+
+    add_node(graph.states)
+    add_node(graph.states)
+    add_node(graph.states)
+    add_node(graph.states)
 }
 
 document.addEventListener('DOMContentLoaded', init);
